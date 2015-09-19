fd4da9f9aebb05928428fd252acc489d3f299c31
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2015-09-19 17:34:10 -0300

    Iniciado a construção do metodo distribuicaoUniforme.

---
+++ Simulação Distribuição/src/br/univali/simulacao/principal/Principal.java
@@ -0,0 +1,13 @@
+
+package br.univali.simulacao.principal;
+
+import br.univali.simulacao.modelo.Distribuicao;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        new Distribuicao().distribuicaoUniforme(5, 2);
+    }
+    
+}
