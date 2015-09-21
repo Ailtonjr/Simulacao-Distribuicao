0b0f8789f07e9e7138a890f0a5b3d386efbcbcc6
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2015-09-21 16:58:11 -0300

    Adicionado metodo distribuicaoUniforme.

--- Simulação Distribuição/src/br/univali/simulacao/modelo/Distribuicao.java
+++ Simulação Distribuição/src/br/univali/simulacao/modelo/Distribuicao.java
@@ -36,12 +36,7 @@ public class Distribuicao {
         double x = m - o;
         
         for (int i = 0;i < tamanho; i++) {
-            lista.add(new Elemento());
-        }
-        
-        for (Elemento l : lista) {
-            l.setValor(x);
-            l.setPercentual((1.0/(o*pow(2.0*Math.PI, (1.0/2.0)))) * pow(Math.E, (pow(x - m, 2)/(2.0*pow(o, 2.0)))* (-1.0)) * 100);
+            lista.add(new Elemento(x, (1.0/(o*pow(2.0*Math.PI, (1.0/2.0)))) * pow(Math.E, (pow(x - m, 2)/(2.0*pow(o, 2.0)))* (-1.0)) * 100));
             x++;
         }
         
@@ -55,8 +50,23 @@ public class Distribuicao {
         return retorno;
     }
     
-    public void distribuicaoUniforme(double a, double b) {
-        // TODO
+    public double distribuicaoUniforme(double a, double b) {
+        // b deve ser o limite superior (caso contrario teria que fazer Math abs nas diferencas)
+        double retorno = 0;
+        int tamanho = (int) (b - a);
+        List<Elemento> lista = new ArrayList();
+        
+        for (int i = 0; i <= b; i++) {
+            lista.add(new Elemento(i + a, 1/(b-a)));    // lista.valor não pode ser incrementado de 1 em 1. Cenario teste a=2.2 b= 3.3
+        }
+        
+        retorno = comparaPercentagem(lista);    // Todas tem chance igual, que numero vem?
+        
+        for (Elemento vetor1 : lista) {
+            System.out.println(vetor1.getValor() + "\t" + vetor1.getPercentual());
+        }
+        
+        return retorno;
     }
     
     
