db0f0863f9d7aa1503cae8fb7bbb44ce9f80e5f6
Ailtonjr <>
2015-09-29 21:23:38 -0300

    Adicionado Conversao de unidades

--- Simulação Distribuição/src/br/univali/simulacao/modelo/Conversor.java
+++ Simulação Distribuição/src/br/univali/simulacao/modelo/Conversor.java
@@ -5,11 +5,11 @@ public class Conversor {
     public static double Conversor(int valor, String unidade) {
         switch (unidade) {
             case "Milisegundo":
-                return valor / 1000;
+                return valor*1000;
             case "Microsegundo":
-                return 1;
+                return valor*1000000;
             case "Nanosegundo":
-                return 2;
+                return valor*1000000000;
             default:
                 return valor;
         }
